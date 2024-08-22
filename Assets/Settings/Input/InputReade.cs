62153b6ee448b62c5dfc6f9d38581fc241f55918
ruddnjs13 <ruddnjs013@gmail.com>
2024-08-23 00:52:15 +0900

    플레이어 완성

--- Assets/Settings/Input/InputReade.cs
+++ Assets/Settings/Input/InputReade.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static Controls;
 
-public class InputReade : ScriptableObject, IPlayerInputActions
+[CreateAssetMenu(menuName ="SO/InputReader")]
+public class InputReader : ScriptableObject, IPlayerInputActions
 {
+    public event Action<Vector3> OnMovementEvent;
+
     private Controls controls;
-    private void Awake()
+    private void OnEnable()
     {
         if (controls == null)
         {
@@ -23,6 +28,38 @@ public class InputReade : ScriptableObject, IPlayerInputActions
         if (context.performed)
         {
             Debug.Log("มกวม");
+        }
+    }
+
+    public void OnUp(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnMovementEvent?.Invoke(new Vector3(0,1,0));
+        }
+    }
+
+    public void OnDown(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            OnMovementEvent?.Invoke(new Vector3(0,-1,0));
+        }
+    }
+
+    public void OnLeft(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnMovementEvent?.Invoke(new Vector3(-1, 0, 0));
+        }
+    }
+
+    public void OnRight(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnMovementEvent?.Invoke(new Vector3(1, 0, 0));
         }
     }
 }
