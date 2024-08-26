e1a4b7073fe077009b9481ae95a7830331c16034
PSB08 <parkseongbin08@gmail.com>
2024-08-26 20:39:51 +0900

    Clear and Gameover UI little complete

---
+++ Assets/Work/PSB/01.Scripts/ResultUIManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _clearPanel;
+    [SerializeField] private GameObject _gameoverPanel;
+
+
+    public static ResultUIManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(Instance);
+        }
+    }
+    private void Start()
+    {
+        Time.timeScale = 1;
+        _clearPanel.SetActive(false);
+        _gameoverPanel.SetActive(false);
+    }
+
+    public void GamaOverUI()
+    {
+        Time.timeScale = 0;
+        _gameoverPanel.SetActive(true);
+    }
+
+    public void GameClearUI()
+    {
+        Time.timeScale = 0;
+        _clearPanel.SetActive(true);
+    }
+
+}
