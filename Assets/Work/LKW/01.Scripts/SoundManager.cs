496a6105d6e30055ab2dda2f784acaeb96c3b907
ruddnjs13 <ruddnjs013@gmail.com>
2024-08-26 23:07:18 +0900

    메인 받기 커밋

--- Assets/Work/LKW/01.Scripts/SoundManager.cs
+++ Assets/Work/LKW/01.Scripts/SoundManager.cs
@@ -1,18 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
+
+public enum Sfx
+{
+
+}
+public enum Bgm
+{
+
+}
+
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _chanelCount;
+
+    [SerializeField] private AudioClip[] _sfxList;
+    [SerializeField] private float _sfxVolume;
+    private AudioSource _sfxPlayer;
+
+    [SerializeField] private AudioClip[] _bgmList;
+    [SerializeField] private float _bgmVolume;
+    private AudioSource _bgmPlayer;
+
+    private Dictionary<Sfx, AudioClip> sfxDic = new Dictionary<Sfx, AudioClip>();
+    private Dictionary<Bgm, AudioClip> bgmDic = new Dictionary<Bgm, AudioClip>();
+
+    public static SoundManager instance;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize()
     {
-        
+
+        //µñ¼î³ë¸®¿¡ ³Ö¾î
+        foreach(AudioClip clip in _sfxList)
+        {
+            sfxDic.Add(Enum.Parse<Sfx>(clip.name), clip);
+        } 
+        foreach(AudioClip clip in _bgmList)
+        {
+            bgmDic.Add(Enum.Parse<Bgm>(clip.name), clip);
+        }
+
+        GameObject obj = new GameObject("BgmPlayer");
+        _bgmPlayer =  obj.AddComponent<AudioSource>();
     }
 }
