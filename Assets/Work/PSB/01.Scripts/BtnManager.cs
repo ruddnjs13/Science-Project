811d129434068a17fc3948d8c04dd9f2f97aa987
PSB08 <parkseongbin08@gmail.com>
2024-08-28 00:43:06 +0900

    .

--- Assets/Work/PSB/01.Scripts/BtnManager.cs
+++ Assets/Work/PSB/01.Scripts/BtnManager.cs
@@ -26,31 +26,31 @@ public class BtnManager : MonoBehaviour
     {
         GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage1");
-        GameManager.Instance._currentStage = 0;
+        GameManager.Instance._currentStage = 1;
     }
     public void Stage2BtnClick()
     {
         GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage2");
-        GameManager.Instance._currentStage = 1;
+        GameManager.Instance._currentStage = 2;
     }
     public void Stage3BtnClick()
     {
         GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage3");
-        GameManager.Instance._currentStage = 2;
+        GameManager.Instance._currentStage = 3;
     }
     public void Stage4BtnClick()
     {
         GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage4");
-        GameManager.Instance._currentStage = 3;
+        GameManager.Instance._currentStage = 4;
     }
     public void Stage5BtnClick()
     {
         GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage5");
-        GameManager.Instance._currentStage = 4;
+        GameManager.Instance._currentStage = 5;
     }
 
     public void NextBtn()
