cfe400afd166d462725239f7e4dbbfce40b5fea0
PSB08 <parkseongbin08@gmail.com>
2024-08-28 08:37:02 +0900

    BtnManager Script some change code
    버튼메니저 스크립트 조금 바꿈

--- Assets/Work/PSB/01.Scripts/BtnManager.cs
+++ Assets/Work/PSB/01.Scripts/BtnManager.cs
@@ -24,31 +24,26 @@ public class BtnManager : MonoBehaviour
 
     public void Stage1BtnClick()
     {
-        GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage1");
         GameManager.Instance._currentStage = 1;
     }
     public void Stage2BtnClick()
     {
-        GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage2");
         GameManager.Instance._currentStage = 2;
     }
     public void Stage3BtnClick()
     {
-        GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage3");
         GameManager.Instance._currentStage = 3;
     }
     public void Stage4BtnClick()
     {
-        GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage4");
         GameManager.Instance._currentStage = 4;
     }
     public void Stage5BtnClick()
     {
-        GameManager.Instance.TimePlay();
         SceneManager.LoadScene("Stage5");
         GameManager.Instance._currentStage = 5;
     }
