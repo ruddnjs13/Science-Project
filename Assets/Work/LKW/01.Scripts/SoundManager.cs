1ffa4a77661133c61a231830ba4de775b7620788
ruddnjs13 <ruddnjs013@gmail.com>
2024-08-27 22:00:25 +0900

    sound

--- Assets/Work/LKW/01.Scripts/SoundManager.cs
+++ Assets/Work/LKW/01.Scripts/SoundManager.cs
@@ -7,21 +7,22 @@ using UnityEngine;
 
 public enum Sfx
 {
-
+    MoveSound
 }
 public enum Bgm
 {
-
+    Bgm2
 }
 
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] private int _chanelCount;
+    [SerializeField] private int _channelCount;
 
     [SerializeField] private AudioClip[] _sfxList;
     [SerializeField] private float _sfxVolume;
-    private AudioSource _sfxPlayer;
+    private AudioSource[] _sfxPlayers;
+    private int channelIndex;
 
     [SerializeField] private AudioClip[] _bgmList;
     [SerializeField] private float _bgmVolume;
@@ -46,6 +47,41 @@ public class SoundManager : MonoBehaviour
         Initialize();
     }
 
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        PlayBgm(Bgm.Bgm2);
+    }
+
+    private void Update()
+    {
+        _bgmPlayer.volume = _bgmVolume;
+        
+    }
+
+    public float BgmVolue
+    {
+        get
+        {
+            return _bgmVolume;
+        }
+        set
+        {
+            _bgmVolume = value;
+        }
+    } 
+    public float SfxVolue
+    {
+        get
+        {
+            return _sfxVolume;
+        }
+        set
+        {
+            _sfxVolume = value;
+        }
+    }
+
     private void Initialize()
     {
 
@@ -56,10 +92,47 @@ public class SoundManager : MonoBehaviour
         } 
         foreach(AudioClip clip in _bgmList)
         {
-            bgmDic.Add(Enum.Parse<Bgm>(clip.name), clip);
+            bgmDic.Add((Bgm)Enum.Parse(typeof(Bgm),clip.name), clip);
         }
 
-        GameObject obj = new GameObject("BgmPlayer");
-        _bgmPlayer =  obj.AddComponent<AudioSource>();
+        GameObject bgmobj = new GameObject("BgmPlayer");
+        bgmobj.transform.parent = transform;
+        _bgmPlayer =  bgmobj.AddComponent<AudioSource>();
+        _bgmPlayer.volume = _bgmVolume;
+        _bgmPlayer.loop = true;
+        _bgmPlayer.playOnAwake = false;
+
+        GameObject sfxobj = new GameObject("SfxPlayer");
+        sfxobj.transform.parent = transform;
+        _sfxPlayers = new AudioSource[_channelCount];
+        for (int i = 0; i< _channelCount; i++)
+        {
+            _sfxPlayers[i] = sfxobj.AddComponent<AudioSource>();
+            _sfxPlayers[i].playOnAwake = false;
+            _sfxPlayers[i].volume = _sfxVolume;
+        }
+    }
+
+    public void PlaySfx(Sfx sfx)
+    {
+        for (int i = 0; i < _sfxPlayers.Length; i++)
+        {
+            int loopIndex = (i + channelIndex) % _sfxPlayers.Length;
+            if (_sfxPlayers[loopIndex].isPlaying)
+            {
+                continue;
+            }
+            channelIndex = loopIndex;
+            _sfxPlayers[loopIndex].clip = _sfxList[(int)sfx];
+            _sfxPlayers[loopIndex].Play();
+            break;
+        }
+    }
+
+
+    public void PlayBgm(Bgm bgm)
+    {
+        _bgmPlayer.clip = bgmDic[bgm];
+        _bgmPlayer.Play();
     }
 }
