d8a7704e93cfd841292a529cc3d7ce0658ca2f90
chuh007 <chhuh007@gmail.com>
2024-08-29 14:48:13 +0900

    .

--- Assets/Work/PSB/01.Scripts/BtnManager.cs
+++ Assets/Work/PSB/01.Scripts/BtnManager.cs
@@ -9,11 +9,13 @@ public class BtnManager : MonoBehaviour
     public void StartGame()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("StageSelectScene");
     }
 
     public void QuitGame()
     {
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         Application.Quit();
 
         #if UNITY_EDITOR
@@ -26,30 +28,35 @@ public class BtnManager : MonoBehaviour
     public void Stage1BtnClick()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Stage1");
         GameManager.Instance._currentStage = 1;
     }
     public void Stage2BtnClick()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Stage2");
         GameManager.Instance._currentStage = 2;
     }
     public void Stage3BtnClick()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Stage3");
         GameManager.Instance._currentStage = 3;
     }
     public void Stage4BtnClick()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Stage4");
         GameManager.Instance._currentStage = 4;
     }
     public void Stage5BtnClick()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Stage5");
         GameManager.Instance._currentStage = 5;
     }
@@ -57,49 +64,59 @@ public class BtnManager : MonoBehaviour
     public void NextBtn()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("StageSelectScene");
     }
 
     public void ExitBtn()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("TitleScene");
     }
 
     public void ReLoadBtn()
     {
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void ClearBtn()
     {
         Time.timeScale = 1;
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("StageClear");
     }
 
     public void TutoGo()
     {
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("TutorialScene");
     }
 
     public void Tuto1Btn()
     {
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Tuto1Scene");
     }
     public void Tuto2Btn()
     {
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Tuto2Scene");
     }
     public void Tuto3Btn()
     {
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Tuto3Scene");
     }
     public void Tuto4Btn()
     {
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Tuto4Scene");
     }
     public void Tuto5Btn()
     {
+        SoundManager.instance.PlaySfx(Sfx.Btn);
         SceneManager.LoadScene("Tuto5Scene");
     }
 }
