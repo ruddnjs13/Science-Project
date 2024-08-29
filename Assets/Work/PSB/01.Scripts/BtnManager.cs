f2ca77bd05d60e22556492b1425a584a2cc9e58b
ruddnjs13 <ruddnjs013@gmail.com>
2024-08-29 11:03:31 +0900

    시간 고침

--- Assets/Work/PSB/01.Scripts/BtnManager.cs
+++ Assets/Work/PSB/01.Scripts/BtnManager.cs
@@ -67,7 +67,6 @@ public class BtnManager : MonoBehaviour
 
     public void ReLoadBtn()
     {
-        GameManager.Instance.TimePlay();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
